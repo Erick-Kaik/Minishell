9f8b85510a432abb6ce4fc140039c6cf6eaf0626
Erick-Kaik <erick.kaik2002@gmail.com>
2023-05-17 17:44:45 -0300

    coisando sinais

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ekaik-ne <ekaik-ne@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: ekaik-ne <ekaik-ne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 10:30:08 by ekaik-ne          #+#    #+#             */
-/*   Updated: 2023/05/16 22:36:56 by ekaik-ne         ###   ########.fr       */
+/*   Updated: 2023/05/17 17:35:17 by ekaik-ne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,40 @@ t_data	g_data;
 
 int main (int argc, char **argv, char **envp)
 {
+    pid_t pid;
+    int stts;
     char *line;
     
+    stts = 0;
     if (argc > 1 || argv == NULL)
         return (0);
     ft_starting_variables(envp);
     ft_start_signals();
     while (1)
     {
-        ft_get_folder();
-        line = readline(" ");
-        if (line == NULL)
-            ft_sig_close(0);
-        ft_check_line(line);
-        ft_clear_struct();
-        free(line);
+        pid = fork();
+        if (pid < 0)
+            continue;
+        else if (pid == 0)
+        {
+            g_data.pid = pid;
+            ft_get_folder();
+            line = readline(" ");
+            if (line == NULL)
+                ft_sig_close(0);
+            ft_check_line(line);
+            ft_clear_struct();
+            free(line);
+            exit(1);
+        }
+        else if (pid > 0)
+        {
+            ft_printf("esperrando\n");
+            waitpid(pid,&stts,WUNTRACED);
+            ft_printf("status global = %d\n", g_data.status);
+            ft_printf("recebeu\n");
+        }
+        ft_printf("Stts = %d\n", stts);
     }
     return (0);
 }
--- signal.c
+++ signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ekaik-ne <ekaik-ne@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: ekaik-ne <ekaik-ne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 10:33:46 by ekaik-ne          #+#    #+#             */
-/*   Updated: 2023/05/16 20:39:30 by ekaik-ne         ###   ########.fr       */
+/*   Updated: 2023/05/17 17:35:07 by ekaik-ne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,20 @@ void ft_start_signals(void)
 void ft_sig_new_prompt(int sig)
 {
     (void)sig;
-	if (g_data.pid > 0)
-        ft_clear_pid(g_data.pid);
-    ft_putchar_fd('\n', 1);
-    rl_replace_line("", 0);
-    if (g_data.pid == 0)
-        ft_get_folder();
-    rl_on_new_line();
-    rl_redisplay();
+    pid_t pid;
+
+    pid = g_data.pid;
+    g_data.pid = 0;
+    if (pid == 0)
+        kill(1, SIGKILL);
+	// if (g_data.pid > 0)
+    //     ft_clear_pid(g_data.pid);
+    // ft_putchar_fd('\n', 1);
+    // rl_replace_line("", 0);
+    // if (g_data.pid == 0)
+    //     ft_get_folder();
+    // rl_on_new_line();
+    // rl_redisplay();
 }
 
 void ft_sig_close(int sig)
@@ -46,7 +52,10 @@ void ft_sig_close(int sig)
     (void)sig;
     ft_putchar('\n');
     ft_clear_struct();
-    exit(0);
+    g_data.status = -1;
+    // kill(-1, SIGKILL);
+    // kill(-1, SIGTERM);
+    kill(1, SIGKILL);
 }
 
 void ft_clear_pid(pid_t pid)
