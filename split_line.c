42b4b6a4fe9fa08e342c1646fd437cd4201c9c8d
Erick-Kaik <erick.kaik2002@gmail.com>
2023-05-26 16:32:33 -0300

    split arrumada

--- split_line.c
+++ split_line.c
@@ -23,23 +23,13 @@ char **ft_broke_line(char *line)
     aux = ft_strtrim(line, " ");
     while (ft_open_quotes(aux) == 1)
         aux = ft_get_more_content(aux, &concat);
-    ft_add_history(aux);
+    add_history(aux);
     count = ft_count_split(aux);
     temp = (char **)malloc(sizeof(char *) * (count + 1));
     temp = ft_split_words(aux, temp, count);
-    // free(aux);
+    free(aux);
     return (temp);
 }
-
-void ft_add_history(char *aux)
-{
-    t_history *new;
-
-    new = ft_new_lst_history(aux);
-    ft_add_lst_history(&g_data.history, new);
-    add_history(aux);
-}
- 
 
 char **ft_split_words(char *str, char **split, int count)
 {
@@ -52,11 +42,9 @@ char **ft_split_words(char *str, char **split, int count)
     while (index < count)
     {
         len = ft_lenth_split(str, &i);
-        ft_printf("len = %d\n", len);
         split[index] = (char *)malloc(sizeof(char) * len);
         ft_fill_split(split[index], str, i, len);
         split[index][len] = '\0';
-        ft_printf("split[%s]\n",split[index]);
         i += ft_fix_index_position(str, i);
         index++;
     }
@@ -67,13 +55,19 @@ char **ft_split_words(char *str, char **split, int count)
 int ft_fix_index_position(char *str, int i)
 {
     int x;
+    char c_red;
 
     x = 0;
-    while (str[i + x] == 32 && str[i + x] != '\0')
+    if (ft_break_redirector(str, i) == 1)
+        c_red = str[i];
+    else
+        c_red = '\0';
+    while ((str[i + x] == ' ' || (c_red != '\0' && str[i + x] == c_red))
+        && str[i + x] != '\0')
         x++;
-    while (str[i + x] != '\0')
+    while (str[i + x] != '\0' && c_red == '\0')
     {
-        if (str[i + x] == 32)
+        if (ft_break_redirector(str, (i + x)) == 1)
             break;
         else if (str[i + x] == 34 || str[i + x] == 39)
             x = ft_jump_quotes(str, str[i + x], (i + x));
@@ -133,7 +127,7 @@ int ft_lenth_split(char *str, int *i)
     len = 0;
     x = 0;
     quotes = 0;
-    while (str[*i] != '\0' && str[*i] == ' ') 
+    while (str[*i] != '\0' && str[*i] == ' ')
         *i += 1;
     while (str[*i + x] != '\0')
     {
@@ -159,10 +153,9 @@ int ft_len_redirector(char *str, int index, int len_backup)
 
     i = 0;
     len = 0;
-    ft_printf("len back = %d\n", len_backup);
     if (len_backup > 0)
         return (len_backup);
-    while (str[index + i] != ' ' && ft_break_redirector(str, index) == 1)
+    while (str[index + i] != ' ' && ft_break_redirector(str, index + i) == 1)
     {
         len++;
         i++;
