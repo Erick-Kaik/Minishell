56d55ffae90c6f1743553dff903968f96c007fc1
Erick-Kaik <erick.kaik2002@gmail.com>
2023-05-26 19:34:09 -0300

    1

--- exec.c
+++ exec.c
@@ -12,64 +12,6 @@
 
 #include "minishell.h"
 
-/* int ft_execute_ft_system(char **line, int *index)
-{
-    char **aux;
-    char *path;
-    int ret;
-    int status;
-    pid_t pid;
-
-    ret = 0;
-    status = 0;    
-    aux = ft_limit_execve(line, index);
-    if (aux == NULL)
-        return (0);
-    path = ft_strjoin("/bin/", aux[0]);
-    pid = fork();
-    if (pid == 0)
-    {
-        if (line[*index] != NULL && ft_its_a_redirector(line[*index], ft_strlen(line[*index])) > 0)
-            ft_redirector(line, index);
-        ret = execve(path, aux, g_data.envp);
-        exit(ret);
-    }
-    else if (pid > 0)
-    {
-        waitpid(pid, &status, WUNTRACED);
-        if (status > 0)
-             ret = -1;
-    }
-    free(path);
-    ft_clear_split_line(aux);
-    return (ret);
-}
-
-char **ft_limit_execve(char **line, int *index)
-{
-    int x;
-    int y;
-    char **aux;
-
-    x = 0;
-    y = 0;
-    while (line[*index + x] != NULL && ft_its_a_redirector(line[*index + x],
-        ft_strlen(line[*index + x])) == 0)
-        x++;
-    aux = (char **)malloc(sizeof(char *) * (x + 1));
-    if (aux == NULL)
-        return (NULL);
-    while (y < x)
-    {
-        aux[y] = ft_strdup(line[*index + y]);
-        y++;
-    }
-    aux[y] = NULL;
-    *index += x;
-    return (aux);
-} */
-
-
 int ft_execute_ft_system(char **line, int *index)
 {
     char **aux;
